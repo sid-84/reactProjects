e9615c718a7928742858ec42f6976533f1dc6990
BscCohort Admin <bsc.cohort.io@gmail.com>
2024-09-28 03:41:07 +0000

    revise and make better

--- axios-lab/src/App.js
+++ axios-lab/src/App.js
@@ -1,15 +1,9 @@
-import './App.css';
-
 import PersonList from './components/PersonList.js';
-import PersonAdd from './components/PersonAdd.js';
-import PersonRemove from './components/PersonRemove.js';
 
 function App() {
   return (
     <div className="App">
-      <PersonAdd />
       <PersonList />
-      <PersonRemove />
     </div>
   );
 }
